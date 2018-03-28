60ac7ae368b1d320f7335aeab8ca5c7fa959679b
Andrew Nikonchuk <anikonch@gmail.com>
2018-03-28 13:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holday}, ${name}!`;
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`;
 }
